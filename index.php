697885ba4317af54fe3c89dcc477a0f8cee192f3
Michael Cinus <michaelcinus@gmail.com>
2020-09-02 09:39:14 +0200

    correzione

--- index.php
+++ index.php
@@ -10,8 +10,11 @@
     
     $text = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio, distinctio minus vero ad a eum magni. Harum quae inventore est, ullam, vitae dolores maxime soluta deleniti aut ex obcaecati labore.';
 
+    $lung = $strlen($text);
     $sText = str_replace ($badwords , "***", $text);
     echo $sText;
+    echo <br>;
+    echo $lung;
     
     ?>
 
